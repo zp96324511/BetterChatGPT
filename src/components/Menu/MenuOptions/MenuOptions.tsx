28312e6808806af5cb2ab76c15bc6868524c072a
阿鹏 <github@buerxing.com>
2023-05-11 14:43:02 +0800

    Update MenuOptions.tsx

--- src/components/Menu/MenuOptions/MenuOptions.tsx
+++ src/components/Menu/MenuOptions/MenuOptions.tsx
@@ -25,11 +25,9 @@ const MenuOptions = () => {
       >
         {countTotalTokens && <TotalTokenCostDisplay />}
         {googleClientId && <GoogleSync clientId={googleClientId} />}
-        <AboutMenu />
         <ImportExportChat />
         <Api />
         <SettingsMenu />
-        <Me />
       </div>
     </>
   );
